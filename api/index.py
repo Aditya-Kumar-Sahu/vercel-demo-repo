8f3c9c7af748005179a71a8f27541cce6a89a1fb
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2025-01-23 16:19:58 +0530

    feat(api): display marks

--- api/index.py
+++ api/index.py
@@ -1,11 +1,23 @@
 # api/index.py
 import json
 from http.server import BaseHTTPRequestHandler
+import pandas as pd
 
+# class handler(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','application/json')
+#         self.end_headers()
+#         self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
+#         return
+    
+df = pd.read_json("./q-vercel-python.json")
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def do_GET(self, x, y):
+        mark_x = df.loc[df['name'] == x]['marks']
+        mark_y = df.loc[df['name'] == y]['marks']
         self.send_response(200)
-        self.send_header('Content-type','application/json')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
+        self.wfile.write(json.dumps({"marks" : [mark_x, mark_y]}).encode('utf-8'))
         return
