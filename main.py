8f88be53f2c152f9d242419fd2ec7d3aacbbac09
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2025-01-23 22:54:44 +0530

    add(file): main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+import pandas as pd
+
+class Output(BaseModel):
+    marks : list[int]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+    )
+
+data = pd.read_json("q-vercel-python.json")
+
+@app.get("/api", response_model=Output)
+async def get_marks(name: List[str] = Query(...)):
+    result = []
+    for n in name:
+        try:
+            result.append(data.loc[data['name']==n]['marks'].iloc[0])
+        except IndexError:
+            pass
+    return {"marks": result}
